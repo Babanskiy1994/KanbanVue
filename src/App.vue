c074f6fba20a565a7c9d77f2b2661c82bcfb9a33
Babanskiy1994 <babanskiy_94@mail.ru>
2019-03-28 16:54:22 +0300

    header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+	<div id="app">
+		<div class="header">
+			<h1>Kanban Board</h1>
+		</div>
+		<router-view/>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-};
+	export default {
+		name: 'app',
+	};
 </script>
+
+<style>
+	.header {
+		text-align: center;
+	}
+</style>
