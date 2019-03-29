4dba270228fe6df17060a4df786e1961bbf09378
Babanskiy1994 <babanskiy_94@mail.ru>
2019-03-29 17:35:43 +0300

    backlog-view

--- src/components/Backlog.vue
+++ src/components/Backlog.vue
@@ -2,10 +2,18 @@
 	<div class="backlog-view">
 		<p>Задачи</p>
 		<new-item></new-item>
+		<br>
+		<div class="card" v-for="item in items" :key="item.id">
+			<div class="card-block">
+				<i class="card-title"><span class="text-muted">#{{item.id}}</span>
+				{{item.text}}</i>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
+	import { mapState } from 'vuex'; 
 	import NewItemForm from './NewItemForm';
 
 	export default {
@@ -13,5 +21,14 @@
 		components: {
 			'new-item': NewItemForm,
 		},
+		computed: mapState({
+			items: s => s.items.todo,
+		}),
 	};
 </script>
+
+<style>
+	.card{
+		margin-bottom: 5px;
+	}
+</style>
