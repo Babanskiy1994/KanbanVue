6b8d62e69ac6de5b6448f6d3228bcb37b87c572e
Babanskiy1994 <babanskiy_94@mail.ru>
2019-03-29 15:13:13 +0300

    form

--- src/components/Backlog.vue
+++ src/components/Backlog.vue
@@ -1,11 +1,17 @@
 <template>
 	<div class="backlog-view">
 		<p>Задачи</p>
+		<new-item></new-item>
 	</div>
 </template>
 
 <script>
+	import NewItemForm from './NewItemForm';
+
 	export default {
 		name: 'Backlog',
+		components: {
+			'new-item': NewItemForm,
+		},
 	};
 </script>
---
+++ src/components/NewItemForm.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="add-item">
+		<form action="#" method="post" v-on:submit.prevent="submitForm">
+			<input type="text" v-model="itemText" placeholder="Добавить задачу...">
+		</form>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'NewItemForm',
+		data() {
+			return {
+				itemText: '',
+			};
+		},
+		methods: {
+			submitForm() {
+				if (this.itemText) {
+					this.$store.commit('addItem', {
+						text: this.itemText,
+					});
+					this.itemText = '';
+				}
+			},
+		},
+	};
+</script>
+
+<style>
+	.add-item input {
+		outline: none;
+		border: 0;
+		border-bottom: 1px solid rgba(0, 0, 0, 0.4);
+		width: 100%;
+		transition: all 0.25s;
+		background: inherit;
+		color: #2F4F4F;
+		box-shadow: none;
+		border-radius: 0;
+	}
+	.add-item input:focus {
+		border-bottom-color: rgba(0, 0, 0, 1);
+	}
+</style>
