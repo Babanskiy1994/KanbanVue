dd5e80f9fe879d887c56cec0d496531ee82b2894
Babanskiy1994 <babanskiy_94@mail.ru>
2019-03-30 17:51:24 +0300

    marker

--- src/components/Backlog.vue
+++ src/components/Backlog.vue
@@ -5,7 +5,7 @@
 		<div class="card" v-for="item in items" :key="item.id">
 			<div class="card-block">
 				<i class="card-title"><span class="text-muted">#{{item.id}}</span>
-				{{item.text}}</i>
+				{{item.text}} <span :class="badgeClass(item)">{{badgeText(item)}}</span></i>
 			</div>
 		</div>
 	</div>
@@ -15,6 +15,20 @@
 	import { mapState } from 'vuex'; 
 	import NewItemForm from './NewItemForm';
 
+	const badgeDetail = {
+		todo: {
+			text: 'сделать',
+			class: 'badge badge-warning',
+		},
+		inProgress: {
+			text: 'в процессе',
+			class: 'badge badge-info',
+		},
+		done: {
+			text: 'сделано',
+			class: 'badge badge-success',
+		},
+	};
 	export default {
 		name: 'Backlog',
 		components: {
@@ -23,6 +37,25 @@
 		computed: mapState({
 			items: s => [...s.items.todo, ...s.items.inProgress, ...s.items.done]
 		}),
+		methods: {
+			itemLane(item) {
+				if (this.$store.state.items.todo.includes(item)) {
+					return 'todo';
+				} else if (this.$store.state.items.inProgress.includes(item)) {
+					return 'inProgress';
+				}
+
+				return 'done';
+			},
+			badgeText(item) {
+				const lane = this.itemLane(item);
+				return badgeDetail[lane].text;
+			},
+			badgeClass(item) {
+				const lane = this.itemLane(item);
+				return badgeDetail[lane].class;
+			},
+		},
 	};
 </script>
 
